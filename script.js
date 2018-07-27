4078e0199a1c70169b7cc6c82cabb7b248f93a1a
Szymon Grabowski <szmiko@wp.pl>
2018-07-27 19:18:11 +0200

    Task end

--- script.js
+++ script.js
@@ -8,6 +8,36 @@ var taxi = {
 	mileage: 281341
 };
 
+var cadi = {
+	make: "GM",
+	model: "Cadillac",
+	year: 1955,
+	color: "jasnobrązowy",
+	passengers: 5,
+	convertible: false,
+	mileage: 12892
+};
+
+var fiat = {
+	make: "Fiat",
+	model: "500",
+	year: 1957,
+	color: "szaroniebieski",
+	passengers: 2,
+	convertible: false,
+	mileage: 88000
+};
+
+var chevy = {
+	make: "Chevy",
+	model: "Bel Air",
+	year: 1957,
+	color: "czerwony",
+	passengers: 2,
+	convertible: false,
+	mileage: 1021
+};
+
 function prequal(car) {
 	if (car.mileage > 100000) {
 		return false;
@@ -17,10 +47,29 @@ function prequal(car) {
 	return true;
 };
 
-worthALook = prequal(taxi);
+worthALook1 = prequal(taxi);
+worthALook2 = prequal(cadi);
+worthALook3 = prequal(fiat);
+worthALook4 = prequal(chevy);
 
-if (worthALook) {
+if (worthALook1) {
 	console.log("Powinieneś zaintereswać się tym " + taxi.make + " " + taxi.model);
 } else {
 	console.log("Ten " + taxi.make + " " + taxi.model + " możesz sobie podarować.");
+};
+
+if (worthALook2) {
+	console.log("Powinieneś zaintereswać się tym " + cadi.make + " " + cadi.model);
+} else {
+	console.log("Ten " + cadi.make + " " + cadi.model + " możesz sobie podarować.");
+};
+if (worthALook3) {
+	console.log("Powinieneś zaintereswać się tym " + fiat.make + " " + fiat.model);
+} else {
+	console.log("Ten " + fiat.make + " " + fiat.model + " możesz sobie podarować.");
+};
+if (worthALook4) {
+	console.log("Powinieneś zaintereswać się tym " + chevy.make + " " + chevy.model);
+} else {
+	console.log("Ten " + chevy.make + " " + chevy.model + " możesz sobie podarować.");
 };
